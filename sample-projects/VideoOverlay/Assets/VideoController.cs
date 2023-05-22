9e032f8ff5547baa7a8cb75ccd7f09521f36a366
hiukim <hiukim528@gmail.com>
2023-05-21 23:51:22 -0700

    preload video overlay

--- sample-projects/VideoOverlay/Assets/VideoController.cs
+++ sample-projects/VideoOverlay/Assets/VideoController.cs
@@ -5,12 +5,17 @@ using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
+    void Awake() {
+        Debug.Log("awake");
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = "./StreamingAssets/video-overlay.mp4";
+        videoPlayer.Prepare();
+    }
     // Start is called before the first frame update
     void Start()
     {
         VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
-        videoPlayer.source = VideoSource.Url;
-        videoPlayer.url = "./StreamingAssets/video-overlay.mp4";
         videoPlayer.Play();
     }
 
